9585c89473f7e89134cd71ff4b6293e2db5e60b8
Maxime Liquet <35924738+maximlt@users.noreply.github.com>
2021-08-31 19:28:46 +0200

    Replace Travis by Github Actions in the dashboard (#23)
    * Update gitignore
    
    * Replace Travis by Github Actions
    
    * Fix pyyaml warning
    
    * Small code clean up
    
    * Update gitignore
    
    * Run the CI on PRs too but don't deploy

--- make_status.py
+++ make_status.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python
 
-import sys
 from jinja2 import Template
-from yaml import load
-import requests
+from yaml import safe_load
 
 with open('dashboard.yml') as f:
-    config = load(f)
-
-existing = {package['repo'].split('/')[1].lower(): package for section in config for package in section['packages']}
-
-# Also get affiliated packages
-registry = []
-# for section in config:
-#     if section['name'] == 'Affiliated Packages':
-#         affiliated = section
-#         break
-# else:
-#     print("Could not find affiliated package section in dashboard.yml")
-#     sys.exit(1)
-
-for package in registry:
-    # FIXME: Not all repo name is actual package name.
-    pkg_name = package['name'].lower()
-
-    if pkg_name in existing:
-        entry = existing[pkg_name]
-    else:
-        entry = {}
-    if 'repo' not in entry:
-        if 'github.com' in package['repo_url']:
-            entry['repo'] = package['repo_url'].split('github.com/')[1]
-        else:
-            print("Skipping package {0} which is not on GitHub".format(package['name']))
-    if 'pypi_name' not in entry:
-        entry['pypi_name'] = package['pypi_name']
-    if 'badges' not in entry:
-        entry['badges'] = 'travis, coveralls, rtd, pypi, conda'
-    # if pkg_name not in existing:
-    #     affiliated['packages'].append(entry)
+    config = safe_load(f)
 
 for section in config:
     for package in section['packages']:
@@ -49,22 +15,22 @@ for section in config:
             package['rtd_name'] = package['name']
         if 'pypi' in package['badges'] and 'pypi_name' not in package:
             package['pypi_name'] = package['name']
+        if 'gha' in package['badges']:
+            if not all(ppt in package for ppt in ['gha_workflow', 'gha_branch']):
+                msg = f'Missing Github Actions workflow file and/or target branch for {package["repo"]}'
+                raise ValueError(msg)
+            package['gha'] = True
         if 'appveyor' in package['badges'] and 'appveyor_project' not in package:
             package['appveyor_project'] = 'holoviz-developers' + '/' + package['name']
         if 'circleci' in package['badges'] and 'circleci_project' not in package:
             package['circleci_project'] = package['repo']
-        if 'travis' in package['badges'] and 'travis_project' not in package:
-            package['travis_project'] = package['repo']
         if 'conda' in package['badges'] and 'conda_channel' not in package:
             package['conda_channel'] = 'pyviz'
         if 'site' in package['badges']:
             package['site_protocol'] = package.get('site_protocol', 'http')
             package['site'] = package.get('site', '{}.pyviz.org'.format(package['name']))
 
-# affiliated['packages'] = sorted(affiliated['packages'], key=lambda x: x['name'].lower())
-
 template = Template(open('template.html', 'r').read())
-
 
 with open('status.html', 'w') as f:
     f.write(template.render(config=config))
