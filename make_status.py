32da20b205aec1e13f528d2d8449a0ccbfdc8e35
maximlt <mliquet@anaconda.com>
2021-08-31 18:00:19 +0200

    Small code clean up

--- make_status.py
+++ make_status.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python
 
-import sys
 from jinja2 import Template
 from yaml import safe_load
-import requests
 
 with open('dashboard.yml') as f:
     config = safe_load(f)
-
-existing = {package['repo'].split('/')[1].lower(): package for section in config for package in section['packages']}
-
-# Also get affiliated packages
-registry = []
-# for section in config:
-#     if section['name'] == 'Affiliated Packages':
-#         affiliated = section
-#         break
-# else:
-#     print("Could not find affiliated package section in dashboard.yml")
-#     sys.exit(1)
-
-for package in registry:
-    # FIXME: Not all repo name is actual package name.
-    pkg_name = package['name'].lower()
-
-    if pkg_name in existing:
-        entry = existing[pkg_name]
-    else:
-        entry = {}
-    if 'repo' not in entry:
-        if 'github.com' in package['repo_url']:
-            entry['repo'] = package['repo_url'].split('github.com/')[1]
-        else:
-            print("Skipping package {0} which is not on GitHub".format(package['name']))
-    if 'pypi_name' not in entry:
-        entry['pypi_name'] = package['pypi_name']
-    if 'badges' not in entry:
-        entry['badges'] = 'travis, coveralls, rtd, pypi, conda'
-    # if pkg_name not in existing:
-    #     affiliated['packages'].append(entry)
 
 for section in config:
     for package in section['packages']:
@@ -64,10 +30,7 @@ for section in config:
             package['site_protocol'] = package.get('site_protocol', 'http')
             package['site'] = package.get('site', '{}.pyviz.org'.format(package['name']))
 
-# affiliated['packages'] = sorted(affiliated['packages'], key=lambda x: x['name'].lower())
-
 template = Template(open('template.html', 'r').read())
-
 
 with open('status.html', 'w') as f:
     f.write(template.render(config=config))
