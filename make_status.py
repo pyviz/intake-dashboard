4e3221c9284df0b800c9cc48ea3b0bb7dd9e76fc
maximlt <mliquet@anaconda.com>
2021-09-14 10:56:26 +0200

    Add a branch entry

--- make_status.py
+++ make_status.py
@@ -11,14 +11,12 @@ for section in config:
         package['user'], package['name'] = package['repo'].split('/')
         package['badges'] = [x.strip() for x in package['badges'].split(',')]
         package['conda_package'] = package.get('conda_package', package['name'])
+        if 'branch' not in package:
+            package['branch'] = 'master'  # Assumes 'master' branch is the default
         if 'rtd' in package['badges'] and 'rtd_name' not in package:
             package['rtd_name'] = package['name']
         if 'pypi' in package['badges'] and 'pypi_name' not in package:
             package['pypi_name'] = package['name']
-        if 'gha' in package['badges']:
-            if not all(ppt in package for ppt in ['gha_workflow', 'gha_branch']):
-                msg = f'Missing Github Actions workflow file and/or target branch for {package["repo"]}'
-                raise ValueError(msg)
             package['gha'] = True
         if 'circleci' in package['badges'] and 'circleci_project' not in package:
             package['circleci_project'] = package['repo']
